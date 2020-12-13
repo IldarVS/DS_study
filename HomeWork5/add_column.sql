7c55108d22966d60f04c63256777f5b3450b239c
IldarVS <32712888+IldarVS@users.noreply.github.com>
2020-12-14 00:32:21 +0500

    Update name table

--- HomeWork5/add_column.sql
+++ HomeWork5/add_column.sql
@@ -1,8 +1,8 @@
 BEGIN TRY  
-     ALTER TABLE computer.dbo.Laptop DROP COLUMN count_column;
+     ALTER TABLE table_name DROP COLUMN count_column;
 END TRY  
 BEGIN CATCH
 --кода нет
 END CATCH;  
-ALTER TABLE computer.dbo.Laptop ADD count_column INT;
-UPDATE computer.dbo.Laptop SET count_column = (SELECT COUNT(*) FROM computer.dbo.Laptop);
+ALTER TABLE table_name ADD count_column INT;
+UPDATE table_name SET count_column = (SELECT COUNT(*) FROM table_name);
